c87260754b70177a2a197a9973895c85d1eccd7c
Femke Knottenbelt <femknotgithub@gmail.com>
2019-04-27 11:53:58 +0000

    Add create schema in program

--- 01_interacting/expenses/schema.sql
+++ 01_interacting/expenses/schema.sql
@@ -1,21 +1,9 @@
-\c postgres
-
-DROP DATABASE IF EXISTS expenses;
-CREATE DATABASE expenses owner "ec2-user";
-
-\c expenses
+SET client_min_messages TO WARNING;
 
 CREATE TABLE expenses(
   id serial PRIMARY KEY,
-  amount numeric(6,2) NOT NULL,
+  amount numeric(6,2) NOT NULL CHECK (amount >= 0.01),
   memo text NOT NULL,
   created_on timestamp DEFAULT NOW()
 );
 
-ALTER TABLE expenses
-  ADD CONSTRAINT positive_amount CHECK (amount >= 0.01);
-
-
-INSERT INTO expenses (amount, memo, created_on) VALUES (14.56, 'Pencils', NOW());
-INSERT INTO expenses (amount, memo, created_on) VALUES (3.29, 'Coffee', NOW());
-INSERT INTO expenses (amount, memo, created_on) VALUES (49.99, 'Text Editor', NOW());
