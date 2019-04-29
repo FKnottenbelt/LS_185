a1e64345a38aba3ac559ef79bce3c5a8bad7d522
Femke Knottenbelt <femknotgithub@gmail.com>
2019-04-29 08:09:16 +0000

    Retrieve todos when retrieving list

--- 02_webapp_db/todos.sql
+++ 02_webapp_db/todos.sql
@@ -1,3 +1,11 @@
 -- test data for todo app
+TRUNCATE TABLE lists RESTART IDENTITY CASCADE;
+
 INSERT INTO lists (name) VALUES ('Homework');
 INSERT INTO lists (name) VALUES ('Groceries');
+
+INSERT INTO todos (name, list_id) VALUES ('Math', 1);
+INSERT INTO todos (name, list_id) VALUES ('Science', 1);
+
+INSERT INTO todos (name, completed, list_id)
+  VALUES ('Art', true, 1);
